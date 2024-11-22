7a3bdc1eee1f52433a439476987dc9acde31cf0f
Diego Dearest <147737067+redvelv18@users.noreply.github.com>
2024-11-22 10:22:36 +0200

    Add files via upload

---
+++ WEB-1k-PD-22112024/basics.js
@@ -0,0 +1,21 @@
+let personAge = 17;
+if (personAge >= 18) {
+    console.log("Pilngadīgs!")
+}
+else {
+    console.log("Nepilngadīgs!")
+};
+
+let namesArray = ["Ara", "Digs", "Choc"]
+
+for(let i = 0; i <= namesArray.length; i++){
+    console.log(namesArray[i]);
+}
+
+
+function sayHello() {
+    let user = "Diego";
+    console.log("Hello," + user)
+}
+
+//come back later
